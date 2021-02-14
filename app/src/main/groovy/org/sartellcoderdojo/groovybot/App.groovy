30d380f036e12b8d4c7f50ba90df971805a901b5
Joe Degiovanni <joe.degiovanni@gmail.com>
2021-02-14 14:48:26 -0600

    February Dojo changes

--- app/src/main/groovy/org/sartellcoderdojo/groovybot/App.groovy
+++ app/src/main/groovy/org/sartellcoderdojo/groovybot/App.groovy
@@ -25,6 +25,7 @@ class App {
             "You couldn't code your way out of a cardboard bag",
             "My grandma writes better code than you, and she's not with us anymore",
             "Your code looks like something that would be typed by a cat walking across the keyboard",
+            "wanna sprite cranberry",
     ]
 
     App(String token = TOKEN) {
@@ -51,16 +52,23 @@ class App {
 
     def handleMessage(MessageCreateEvent event) {
         Message message = event.getMessage()
-        log.info("Recieved message: ${event.message.content}")
-        MessageChannel channel = event.message.channel.block()
-        log.info("from channel ${channel.name}: $channel")
-        switch (message.content) {
-            case "!ping":
-                replyTo(message, "Pong!")
-                break
-            case "!insult":
-                replyTo(message, INSULTS[Math.random() * INSULTS.size()])
-                break
+        def authorName = message.getAuthor().get().getUsername()
+        if (authorName != 'Groovy Bot') {
+          log.info("Recieved message: ${event.message.content}")
+          MessageChannel channel = event.message.channel.block()
+          log.info("from channel ${channel.name}: $channel")
+          switch (message.content) {
+              case "!ping":
+                  replyTo(message, "Pong!")
+                  break
+              case "!insult":
+                  replyTo(message, INSULTS[Math.random() * INSULTS.size()])
+                  break
+          }
+          if (message.content.toLowerCase() ==~ /.*v[aeiou]l[aeiou]nt[aeiou]n[aeiou]?s.*/) {
+            replyTo(message, '***Happy*** Valentine\'s Day! ❤️❤️❤️❤️❤️')
+            replyTo(message, 'https://giphy.com/embed/26DNdwaQd703CekaQ')
+          }
         }
     }
 
